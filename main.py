c2c5249a37767f1d4ef46eab03200c9daef06776
WillPiledriver <willpiledriver@gmail.com>
2023-07-31 00:46:59 -0400

    i aint never crossed a man that didnt deserve it

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import threading
 from global_hotkeys import *
 import numpy as np
 from sklearn.cluster import DBSCAN
-import subprocess
 import socketio
 import asyncio
 import cv2
@@ -14,6 +13,8 @@ import json
 import pygetwindow as gw
 import math
 
+
+# TODO: viewport/scale calculations for accurate clicking
 runelite_ws_url = "ws://localhost:8085"
 runelite_ws = rp(runelite_ws_url)
 sio = socketio.AsyncClient()
@@ -28,13 +29,16 @@ click_cooldown = 2.5
 channel = "thepiledriver"
 heat_map = True
 command_chance = 1.0
-command_cooldown = 5
+command_cooldown = 0
 channel_id = "23728793"
+block_mouse = False
 
 
 load_dotenv()
-user_dict = dict()
+click_dict = dict()
+cmd_dict = dict()
 auth_list = list()
+
 x_coordinates = np.array([])
 y_coordinates = np.array([])
 
@@ -56,15 +60,15 @@ async def message(data):
 
 @sio.event
 async def leftClick(data):
-    global x_coordinates, y_coordinates, user_dict
+    global x_coordinates, y_coordinates, click_dict
     size_x, size_y = pydirectinput.size()
-    '''if data["opaque_id"] in user_dict:
-        if user_dict[data["opaque_id"]]["time"] + click_cooldown > data["event_time"]:
+    '''if data["opaque_id"] in click_dict:
+        if click_dict[data["opaque_id"]]["time"] + click_cooldown > data["event_time"]:
             return'''
     
     x, y = int(data["x"] * size_x), int(data["y"] * size_y)
     if not mask[y, x].any():
-        user_dict[data["opaque_id"]] = {
+        click_dict[data["opaque_id"]] = {
             "time": data["event_time"],
             "button": "left",
             "coords": (x, y)
@@ -72,15 +76,15 @@ async def leftClick(data):
 
 @sio.event
 async def rightClick(data):
-    global x_coordinates, y_coordinates, user_dict
+    global x_coordinates, y_coordinates, click_dict
     size_x, size_y = pydirectinput.size()
-    '''if data["opaque_id"] in user_dict:
-        if user_dict[data["opaque_id"]]["time"] + click_cooldown > data["event_time"]:
+    '''if data["opaque_id"] in click_dict:
+        if click_dict[data["opaque_id"]]["time"] + click_cooldown > data["event_time"]:
             return'''
     
     x, y = int(data["x"] * size_x), int(data["y"] * size_y)
     if not mask[y, x].any():
-        user_dict[data["opaque_id"]] = {
+        click_dict[data["opaque_id"]] = {
             "time": data["event_time"],
             "button": "right",
             "coords": (x, y)
@@ -104,70 +108,89 @@ async def run_socketio():
     await sio.connect("https://willpile.com:8080")
     await sio.wait()
 
-def callback(data):
-    global x_coordinates, y_coordinates, user_dict
-    size_x, size_y = pydirectinput.size()
-    if data["opaque_id"] in user_dict:
-        if user_dict[data["opaque_id"]]["time"] + click_cooldown > data["event_time"]:
-            return
-    
-    x, y = int(data["x"] * size_x), int(data["y"] * size_y)
-    user_dict[data["opaque_id"]] = {
-        "time": data["event_time"],
-        "coords": (x, y)
-    }
-
-def mouse_loop():
-    global x_coordinates, y_coordinates, user_dict
+async def interaction_loop():
+    global x_coordinates, y_coordinates, click_dict, block_mouse, cmd_dict
     while True:
-        time.sleep(click_cooldown)
-        if len(user_dict) == 0:
+        await asyncio.sleep(click_cooldown / 2)
+        if len(click_dict) + len(cmd_dict) == 0:
             continue
-        
-        x_coordinates = np.array([data["coords"][0] for user, data in user_dict.items()])
-        y_coordinates = np.array([data["coords"][1] for user, data in user_dict.items()])
+        await mouse_loop()
+        await asyncio.sleep(click_cooldown / 2)
+        await cmd_loop()
 
-        coordinates = np.column_stack((x_coordinates, y_coordinates))
 
-        # Define the DBSCAN parameters
-        eps = 150  # Distance threshold for clustering
-        min_samples = 1  # Minimum number of points in a cluster
+async def mouse_loop():
+    global x_coordinates, y_coordinates, click_dict, block_mouse
+    
+    if len(click_dict) == 0:
+        return
+    x_coordinates = np.array([data["coords"][0] for user, data in click_dict.items()])
+    y_coordinates = np.array([data["coords"][1] for user, data in click_dict.items()])
 
-        # Perform DBSCAN clustering
-        dbscan = DBSCAN(eps=eps, min_samples=min_samples)
-        labels = dbscan.fit_predict(coordinates)
+    coordinates = np.column_stack((x_coordinates, y_coordinates))
 
-        # Find the cluster with the most points
-        unique_labels, counts = np.unique(labels, return_counts=True)
-        most_populated_cluster_label = unique_labels[np.argmax(counts)]
+    # Define the DBSCAN parameters
+    eps = 150  # Distance threshold for clustering
+    min_samples = 1  # Minimum number of points in a cluster
 
-        # Get the center coordinates of the most populated cluster
-        most_populated_cluster = coordinates[labels == most_populated_cluster_label]
-        center = np.mean(most_populated_cluster, axis=0)
+    # Perform DBSCAN clustering
+    dbscan = DBSCAN(eps=eps, min_samples=min_samples)
+    labels = dbscan.fit_predict(coordinates)
 
-        # Count the clicks of the most populated cluster
-        click_counts = {
-            "left": 0,
-            "right": 0
-        }
-        for i, (user, data) in enumerate(user_dict.items()):
-            if labels[i] == most_populated_cluster_label:
-                button = data["button"]
-                if button in click_counts:
-                    click_counts[button] += 1
-        most_clicked_key = max(click_counts, key=click_counts.get)
+    # Find the cluster with the most points
+    unique_labels, counts = np.unique(labels, return_counts=True)
+    most_populated_cluster_label = unique_labels[np.argmax(counts)]
 
-        # Human-like mouse movement and click
-        x, y = (round(center[0]), round(center[1]))
-        print("Center coordinates:", x, y)
-        if mask[y, x].any():
-            return
-        r = random.uniform(0.5, 1.25)
-        user_dict = dict()
-        subprocess.call(["python", "human_mouse.py", str(x), str(y), str(r), most_clicked_key])
-        time.sleep(random.uniform(0.2, 0.7))
-        time.sleep(click_cooldown)
+    # Get the center coordinates of the most populated cluster
+    most_populated_cluster = coordinates[labels == most_populated_cluster_label]
+    center = np.mean(most_populated_cluster, axis=0)
 
+    # Count the clicks of the most populated cluster
+    click_counts = {
+        "left": 0,
+        "right": 0
+    }
+    for i, (user, data) in enumerate(click_dict.items()):
+        if labels[i] == most_populated_cluster_label:
+            button = data["button"]
+            if button in click_counts:
+                click_counts[button] += 1
+    most_clicked_key = max(click_counts, key=click_counts.get)
+
+    # Human-like mouse movement and click
+    # TODO: scale x, y to window size
+    x, y = (round(center[0]), round(center[1]))
+    print("Center coordinates:", x, y)
+    if mask[y, x].any():
+        return
+    click_dict = dict()
+    await clicky(x, y, most_clicked_key)
+
+def most_common_action_query_pair(cmd_dict):
+    action_query_count = {}
+
+    # Count occurrences of each action-query pair
+    for user, cmd in cmd_dict.items():
+        action_query = (cmd["action"], cmd["query"].lower())
+        action_query_count[action_query] = action_query_count.get(action_query, 0) + 1
+
+    # Find the most common action-query pair
+    most_common_pair = max(action_query_count, key=action_query_count.get)
+    result = {
+        "action": most_common_pair[0],
+        "query": most_common_pair[1]
+    }
+    return result
+
+async def cmd_loop():
+    global cmd_dict
+    if len(cmd_dict) == 0:
+        return
+    j = most_common_action_query_pair(cmd_dict)
+    print(j)
+    await runelite_ws.send(json.dumps(j))
+    cmd_dict = dict()
+       
 # Load the PNG image
 image = cv2.imread(image_path)
 
@@ -247,15 +270,31 @@ bot.chance = command_chance
 bot.cooldown = command_cooldown
 # bot.cmd["!help"] = help_cmd
 
-def clicky(x, y, button="left", steady=False):
+async def clicky(x, y, button="left", steady=False):
+    global block_mouse
     window = get_window("runelite")
     offsets = window.topleft
     pos = pydirectinput.position()
     distance = math.sqrt((x - pos[0])**2 + (y - pos[1])**2)
     fraction = distance / 1100
     r = max(random.uniform(0.5, 1) * fraction, random.uniform(0.05, 0.17))
-    subprocess.call(["python", "human_mouse.py", str(int(x) + offsets[0]), str(int(y) + offsets[1] + window_y_offset), str(r), button, str(int(steady))])
-
+    
+    cmd = [
+        "python",
+        "human_mouse.py",
+        str(int(x) + offsets[0]),
+        str(int(y) + offsets[1] + window_y_offset),
+        str(r),
+        button,
+        str(int(steady))
+    ]
+    while block_mouse:
+        await asyncio.sleep(0.1)
+        print("blocking mouse")
+    block_mouse = True
+    process = await asyncio.create_subprocess_exec(*cmd)
+    await process.wait()  # Wait for the subprocess to finish
+    block_mouse = False
 
 @bot.command(name="help")
 async def help_cmd(ctx: commands.Context):
@@ -265,26 +304,85 @@ async def help_cmd(ctx: commands.Context):
 
 @bot.command(name="drop")
 async def drop_all(ctx: commands.Context):
+    legal = ["logs", "shrimp", "trout", "salmon", "bones", "ore"]
+    global cmd_dict
+    q = " ".join(ctx.message.content.split()[1:])
+    is_legal = False
+    for l in legal:
+        if l in q.lower():
+            is_legal = True
+            break
+    if not is_legal:
+        return
     j = {
         "action": "drop",
+        "query": q
+    }
+    cmd_dict[ctx.message.author] = j
+    # await runelite_ws.send(json.dumps(j))
+
+@bot.command(name="loot")
+async def tile(ctx: commands.Context):
+    global cmd_dict
+    j = {
+        "action": "loot",
         "query": " ".join(ctx.message.content.split()[1:])
     }
-    await runelite_ws.send(json.dumps(j))
+    cmd_dict[ctx.message.author] = j
 
 @bot.command(name="npc")
 async def click_npc(ctx: commands.Context):
+    global cmd_dict
     j = {
         "action": "npc",
         "query": " ".join(ctx.message.content.split()[1:])
     }
-    await runelite_ws.send(json.dumps(j))
+    cmd_dict[ctx.message.author] = j
+    # await runelite_ws.send(json.dumps(j))
+
+@bot.command(name="tile")
+async def tile(ctx: commands.Context):
+    global cmd_dict
+    j = {
+        "action": "tile",
+        "query": " ".join(ctx.message.content.split()[1:])
+    }
+    cmd_dict[ctx.message.author] = j
+    # await runelite_ws.send(json.dumps(j)) 
+
+@runelite_ws.event(name="tile")
+async def tile_rcv(data):
+    data = data["response"]
+    x = [coord for coord in data["x"] if coord != 0]
+    y = [coord for coord in data["y"] if coord != 0]
+    if len(x) > 0:
+        centroid_x = sum(x) / len(x)
+        centroid_y = sum(y) / len(y)
+        await clicky(centroid_x, centroid_y)
+        print("Centroid coordinates: ({}, {})".format(int(centroid_x), int(centroid_y)))
+
+@runelite_ws.event(name="loot")
+async def tile_rcv(data):
+    data = data["response"]
+    print(data)
+    x = [coord for coord in data["x"] if coord != 0]
+    y = [coord for coord in data["y"] if coord != 0]
+    if len(x) > 0:
+        centroid_x = sum(x) / len(x)
+        centroid_y = sum(y) / len(y)
+        await clicky(centroid_x, centroid_y)
+        print("Centroid coordinates: ({}, {})".format(int(centroid_x), int(centroid_y)))
+
 
 @runelite_ws.event(name="drop")
 async def drop_rcv(data):
     data = data["response"]
+    r = random.uniform(0.095, 0.21)
+    await bot.send_input("esc", r)
+    await asyncio.sleep(random.uniform(0.05, 0.15))
     await bot.send_input("shift", True)
     for i in range(len(data)):
-        clicky(data[i]["x"] + random.randint(-3, 3), data[i]["y"] + random.randint(-3, 3), steady=True)
+        await clicky(data[i]["x"] + random.randint(-3, 3), data[i]["y"] + random.randint(-3, 3), steady=True)
     await bot.send_input("shift", False)
 
 @runelite_ws.event(name="npc")
@@ -294,8 +392,7 @@ async def npc_rcv(data):
     y = [coord for coord in data["response"]["y"] if coord != 0]
     centroid_x = sum(x) / len(x)
     centroid_y = sum(y) / len(y)
-    r = random.uniform(0.5, 1.25)
-    clicky(centroid_x, centroid_y)
+    await clicky(centroid_x, centroid_y)
     print("Centroid coordinates: ({}, {})".format(int(centroid_x), int(centroid_y)))
 
 async def kill_script():
@@ -307,6 +404,10 @@ def on_hotkey_press():
 
 def run_s():
     asyncio.run(run_socketio())
+
+def run_interaction_loop():
+    asyncio.run(interaction_loop())
+
 
 async def main():
     bindings = [{
@@ -323,7 +424,7 @@ async def main():
         thread2.start()"""
         thread2 = threading.Thread(target=run_s)
         thread2.start()
-        thread3 = threading.Thread(target=mouse_loop)
+        thread3 = threading.Thread(target=run_interaction_loop)
         thread3.start()
     loop = asyncio.get_event_loop()
     register_hotkeys(bindings)
